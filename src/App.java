b23e8176d7a480c0b5bbe6d7f8c3f6efee51c284
Wilder Hernández Castro <whernandez93@ucatolica.edu.co>
2021-06-29 21:01:30 -0500

    second class, java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
+
+    
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int suma = 5+12;
+        System.out.println(suma);
+        
+        
     }
 }
